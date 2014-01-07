f0b927be64a6a0bad0bd556be3b6b36f7890ae65
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-06 16:45:09 -0800

    Removed fileread directive

--- app/controllers/frankie.js
+++ app/controllers/frankie.js
@@ -1,23 +1,5 @@
 var frankieApp = angular.module('frankieApp', ['hmTouchevents']);
 
-frankieApp.directive("fileread", [function () {
-    return {
-        scope: {
-            fileread: "="
-        },
-        link: function (scope, element, attributes) {
-            element.bind("change", function (changeEvent) {
-                var reader = new FileReader();
-                reader.onload = function (loadEvent) {
-                    scope.$apply(function () {
-                        scope.fileread = loadEvent.target.result;
-                    });
-                }
-                reader.readAsDataURL(changeEvent.target.files[0]);
-            });
-        }
-    }
-}]);
 // Index: http://localhost/views/frankie/index.html
 
 frankieApp.controller('IndexCtrl', function ($scope) {
