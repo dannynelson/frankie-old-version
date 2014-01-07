20123e7aa896c8f2ffbf869584287e4cbb4d1672
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-06 21:37:57 -0800

    Added today factory

--- app/controllers/frankie.js
+++ app/controllers/frankie.js
@@ -18,3 +18,13 @@ frankieApp.directive("fileread", [function () {
     }
   };
 }]);
+
+frankieApp.factory('today', function() {
+    // generate current date
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+  if (dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = yyyy+'-'+mm+'-'+dd;
+  return today;
+});
