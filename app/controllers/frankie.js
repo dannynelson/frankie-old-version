9c5db7dc69baf524f58f547ed69ceb3f09d05f94
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-10 09:54:52 -0800

    Refactored navigation factory

--- app/controllers/frankie.js
+++ app/controllers/frankie.js
@@ -32,15 +32,15 @@ frankieApp.factory('today', function() {
 });
 
 frankieApp.factory('navigation', function() {
-  var rightButton, leftButton;
-  var setRight = function(right, action) {
-    rightButton = new steroids.buttons.NavigationBarButton();
-    if (right.slice(right.length-4) === '.png') {
-      rightButton.imagePath = right;
+
+  var setButton = function(title, action) {
+    button = new steroids.buttons.NavigationBarButton();
+    if (title.slice(title.length-4) === '.png') {
+      button.imagePath = title;
     } else {
-      rightButton.title = right;
+      button.title = title;
     }
-    rightButton.onTap = function() {
+    button.onTap = function() {
       // if it is a url string, push it to the layer stack
       if (typeof(action) === 'string') {
         var newView = new steroids.views.WebView(action);
@@ -50,31 +50,19 @@ frankieApp.factory('navigation', function() {
         action();
       }
     };
+    return button;
   };
-  var setLeft = function(left, action) {
-    leftButton = new steroids.buttons.NavigationBarButton();
-    if (left.slice(left.length-4) === '.png') {
-      leftButton.imagePath = left;
-    } else {
-      leftButton.title = left;
-    }
-    leftButton.onTap = function() {
-      // if it is a url string, push it to the layer stack
-      if (typeof(action) === 'string') {
-        var newView = new steroids.views.WebView(action);
-        steroids.layers.push(newView);
-      // otherwise, execute the action
-      } else {
-        action();
-      }
-    };
-  };
+
   // expects right.title and right.pagePath, left.title and left.action
   var buildNav = function(title, right, left) {
     steroids.view.navigationBar.show(title);
     // action can be function or url to push
-    if (right) setRight(right.title, right.action);
-    if (left) setLeft(left.title, left.action);
+    if (right) {
+      var rightButton = setButton(right.title, right.action);
+    }
+    if (left) {
+      var leftButton = setButton(left.title, left.action);
+    }
     if (right && !left) {
       steroids.view.navigationBar.setButtons({
         right: [rightButton]
