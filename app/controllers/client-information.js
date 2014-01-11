9a4b5e8b27948f5e468a43e326db592be7d049ab
Daniel Nelson <danielnelsonguitar@gmail.com>
2014-01-10 17:23:38 -0800

    Back button automatically saves

--- app/controllers/client-information.js
+++ app/controllers/client-information.js
@@ -1,12 +1,19 @@
-frankieApp.controller('ClientCtrl', function ($scope) {
-  steroids.view.navigationBar.show('Client Info');
-
-  $scope.client = JSON.parse(localStorage.getItem("clientInfo")) || {};
-
+frankieApp.controller('ClientCtrl', function ($scope, navigation) {
+  
   $scope.create = function(input) {
     localStorage.setItem("clientInfo", JSON.stringify(input));
     var msg = { status: 'clientUpdated' };
     window.postMessage(msg, "*");
     steroids.layers.pop();
   };
+
+  navigation.build(
+    'Client Info',
+    {title: '', action: function() { $scope.create($scope.client);} } ,
+    {title: '/icons/left.png', action: function() {$scope.create($scope.client);} }
+  );
+
+  $scope.client = JSON.parse(localStorage.getItem("clientInfo")) || {};
+
+
 });
--- app/controllers/timeline.js
+++ app/controllers/timeline.js
@@ -1,8 +1,16 @@
 frankieApp.controller('TimelineCtrl', function ($scope, today, navigation) {
 
+  $scope.save = function() {
+    localStorage.setItem("timeline", angular.toJson($scope.milestones));
+    var msg = { status: 'timelineUpdated' };
+    window.postMessage(msg, "*");
+    steroids.layers.pop();
+  };
+
   navigation.build(
     'Timeline',
-    {title: '/icons/plus.png', action: function() { $scope.$apply($scope.addMilestone()); }}
+    {title: '/icons/plus.png', action: function() { $scope.$apply($scope.addMilestone()); }},
+    {title: '/icons/left.png', action: $scope.save }
   );
 
   $scope.milestones = JSON.parse(localStorage.getItem('timeline')) || [{ description: "", date: today }];
@@ -14,10 +22,5 @@ frankieApp.controller('TimelineCtrl', function ($scope, today, navigation) {
     });
   };
 
-  $scope.save = function() {
-    localStorage.setItem("timeline", angular.toJson($scope.milestones));
-    var msg = { status: 'timelineUpdated' };
-    window.postMessage(msg, "*");
-    steroids.layers.pop();
-  };
+
 });
